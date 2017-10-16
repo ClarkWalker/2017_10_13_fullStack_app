7eafbdc797b953d6cb4b0c59216f6182fac8c403
Clark Walker <303walker@gmail.com>
2017-10-16 14:00:41 -0600

    backing stuff up

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 // Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
   development: {
@@ -7,7 +8,10 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL + '?ssl=true'
-  }
+    // connection: process.env.DATABASE_URL + '?ssl=true'
+    // connection: process.env.DATABASE_URL + 'https://cws-fullstackapp.herokuapp.com/' + '?ssl=true'
+    // connection: 'postgres://kihnrktpvxmemj:d705f319be9ee719ddb8bb617840f7e59363e7e535e0bb3231e5ca2c56801567@ec2-23-23-244-83.compute-1.amazonaws.com'
+    connection: process.env.DATABASE_URL
+  },
 
 };
